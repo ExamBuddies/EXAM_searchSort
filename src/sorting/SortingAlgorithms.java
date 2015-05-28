17230e1e6266117955c142c8c947b514d1a2da23
DAT14V1MadsPelt <mads3166@stud.kea.dk>
2015-05-28 18:17:47 +0200

    insertion sort. Stadig kun på int arrays og iterativt

--- src/sorting/SortingAlgorithms.java
+++ src/sorting/SortingAlgorithms.java
@@ -4,23 +4,38 @@ public class SortingAlgorithms {
 	
 	// Selection Sort http://www.java2novice.com/java-sorting-algorithms/selection-sort/
 	// indtil videre bare med ints.
+	// Udvid evt med rekusriv algoritme
 	public static int[] selectionSort(int[] arr){
         
-		for (int i = 0; i < arr.length - 1; i++)
+		for (int i = 0; i < arr.length - 1; i++) // outer loop, through all elements
         {
-            int index = i;
-            for (int j = i + 1; j < arr.length; j++)
-                if (arr[j] < arr[index]) 
-                    index = j;
-      
-            int smallerNumber = arr[index];  
-            arr[index] = arr[i];
-            arr[i] = smallerNumber;
+            int index = i; // set index to current position in outer loop
+            for (int j = i + 1; j < arr.length; j++) // inner loop through all elements larger than i
+                if (arr[j] < arr[index])
+                {
+                    index = j;  // set index to index of lowest value
+                }
+            int smallerNumber = arr[index];  // smallest value in array
+            arr[index] = arr[i];    // swap the current  
+            arr[i] = smallerNumber; // and the lowest value
         }
         return arr;
     }
 	
 	// Insertion sort
+	// https://www.youtube.com/watch?v=DFG-XuyPYUQ <- han er rimelig retard, men brugbar alligevel.
+	public static int[] insertionSort(int[] arr){
+		for (int i = 1; i < arr.length-1; i++){ // Det første element i array er undtaget - det ses som det første sorterede element.
+			int element = arr[i]; // værdien af det nuværende index
+			int j = i;            // j sættes lige den samme værdi som det nuværende index
+			while(j > 0 && arr[j-1] > element){  // mens j er større end 0, og værdien af arr[j-1] er er større end den vi prøver at sortere 
+				arr[j] = arr[j-1]; 
+				j--;
+			}
+			arr[j] = element;
+		}
+		return arr;
+	}
 	
 	// Bubble sort
 	
--- src/sorting/SortingDemo.java
+++ src/sorting/SortingDemo.java
@@ -1,20 +1,33 @@
 package sorting;
 
 import java.util.Arrays;
+import java.util.Collections;
 
 public class SortingDemo {
 	public static void main(String[] args){
-		int[] arr = {6,2,0,-1,20,1,7,3,4,19,6,5,-2,8,7,11,9,-3,12,17,15,16,13};
-		System.out.println("Array before sorting: " + Arrays.toString(arr));
-		
+		int[] shortArr = {6,2,0,-1,20,1,7,3,4,19,6,5,-2,8,7,11};
+		int[] longArr = new int[2000];
+		for(int i = 0; i < 2000; i++){
+			longArr[i] = (int) (Math.random() * 100);
+		}
+		System.out.println("Array before sorting: " + Arrays.toString(shortArr));		
 		
 		// Selection sort
+//		long millisBefore = System.currentTimeMillis(); // time before sort
+//		longArr = SortingAlgorithms.selectionSort(longArr);
+//		long sortTime = System.currentTimeMillis() - millisBefore; // Finder tid det tog at sortere ved at trække nuværende time fra før sortering.
+//		
+//		int[] selectionSortArr = SortingAlgorithms.selectionSort(shortArr);
+//		System.out.println("Array after selection sort: " + Arrays.toString(selectionSortArr) + ".\nSorting 2000 elements took " + sortTime + " milliseconds.");
+		
+		
+		// Insertion sort
 		long millisBefore = System.currentTimeMillis(); // time before sort
-		int[] selectionSortArr = SortingAlgorithms.selectionSort(arr);
+		longArr = SortingAlgorithms.insertionSort(longArr);
 		long sortTime = System.currentTimeMillis() - millisBefore; // Finder tid det tog at sortere ved at trække nuværende time fra før sortering.
-		System.out.println("Array after selection sort: " + Arrays.toString(selectionSortArr) + ". It took " + sortTime + " milliseconds.");
 		
-		
+		int[] insertionSortArr = SortingAlgorithms.insertionSort(shortArr);
+		System.out.println("Array after insertion sort: " + Arrays.toString(insertionSortArr) + ".\nSorting 2000 elements took " + sortTime + " milliseconds.");
 	}
 	
 	
