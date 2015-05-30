6b01e9019b3e501f2566d3375c34ffa64f48497f
DAT14V1 - Casper Vejdiksen <casp539b@stud.kea.dk>
2015-05-30 16:43:12 +0200

    Create SequentialSearch.java

---
+++ src/search/SequentialSearch.java
@@ -0,0 +1,22 @@
+public class SequentialSearch {
+
+	public static void main(String[] args){
+
+		int[] arr = {1, 27, 0, -6, 123};
+		int value = 0;
+
+		System.out.println(sequentialSearch(arr, value));
+	}
+
+	public static int sequentialSearch(int[] arr, int value) {
+
+		for (int i = 0; i < arr.length; i++) {
+
+			if(arr[i] == value) {
+				return i;
+			}
+		}
+		return -1;
+	}
+
+} 
